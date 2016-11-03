b2692a1e677137a9ac8cdeb515daa3a0f5253f0d
Junhak Lee <ejh2163@gmail.com>
2016-11-03 18:21:58 +0000

    view template and logic, changes to posts template

--- run.py
+++ run.py
@@ -2,10 +2,11 @@ import os
 from project import app
 
 if __name__ == "__main__":
-    #app.run()
-    app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
+    app.run()
+    #app.run(host = os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
     
     # export APP_SETTINGS='config.DevConfig'
+    # export DATABASE_URL='mysql://jlee7737:@localhost/calbang'
     # sudo service mysql start
     # python run.py
     
